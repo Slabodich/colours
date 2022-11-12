2c7e68ffaefba0815ac86b6e87984ab169cc4aa1
George <77537407+Slabodich@users.noreply.github.com>
2022-11-12 14:56:51 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+const cols = document.querySelectorAll('.col')
+
+addEventListener('keydown', (event) => {
+    event.preventDefault()
+    if(event.code.toLowerCase() === 'space'){
+        setRandomColors()
+    }
+})
+
+addEventListener('click', event =>{
+    const type = event.target.dataset.type
+
+    if(type === 'lock'){
+        const node = 
+         event.target.tagName.toLowerCase() === 'i' 
+          ? event.target
+          : event.target.children[0]
+    
+          node.classList.toggle('fa-lock-open')
+          node.classList.toggle('fa-lock')
+    } else if(type === 'copy'){
+        copyToClipboard(event.target.textContent)
+    }
+
+})
+
+function generateRandomColor(){
+    const hexColor = '0123456789ABCDEF'
+    let color = ''
+    for (let i = 0; i < 6; i++){
+        color += hexColor[Math.floor(Math.random() * hexColor.length)]
+    }
+    return '#' + color
+}
+
+function copyToClipboard(text){
+    return navigator.clipboard.writeText(text)
+}
+
+function setRandomColors(){
+    cols.forEach((col) => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
+        const text = col.querySelector('h2')
+        const button = col.querySelector('button')
+        const color = generateRandomColor()
+
+        if(isLocked){
+            return
+        }
+
+        text.textContent = color
+        col.style.background = color
+        setTextColor(text, color)
+        setTextColor(button, color)
+    })
+}
+
+function setTextColor(text, color){
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+
+setRandomColors()
